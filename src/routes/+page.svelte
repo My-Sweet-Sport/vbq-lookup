0df46458326b0ba36fdb347c0326bb5d317336ea
Régis Tremblay Lefrançois <regg00@gmail.com>
2025-10-27 11:28:54 -0400

    working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,24 +34,31 @@
 	};
 </script>
 
-<form class="flex w-1/2 flex-col items-center space-y-4" onsubmit={handleSubmit}>
-	<div class="flex w-full items-center justify-center space-x-2">
-		<Input
-			type="text"
-			placeholder="Code Postal"
-			required
-			bind:value={postalCode}
-			autocomplete="postal-code"
-		/>
-		<Button type="submit">Rechercher</Button>
-	</div>
+<div class="absolute left-[5px] top-[5px]">
+	<img src="/logo.png" alt="VBQ Lookup" class="h-12 w-auto" />
+</div>
 
-	{#if result}
-		<div class="w-full rounded-lg bg-white/10 p-4 text-center">
-			<p class="text-lg font-semibold">{result['Region Espoir']}</p>
-			<p class="text-sm text-gray-500">{result.Ville}</p>
+<div class="flex min-h-screen items-start justify-center pt-[calc(33vh)]">
+	<form class="flex w-full max-w-2xl flex-col items-center space-y-6" onsubmit={handleSubmit}>
+		<div class="flex w-full items-center justify-center space-x-3">
+			<Input
+				type="text"
+				placeholder="Code Postal"
+				required
+				bind:value={postalCode}
+				autocomplete="postal-code"
+				class="h-12 text-lg"
+			/>
+			<Button type="submit" class="h-12 text-lg">Rechercher</Button>
 		</div>
-	{:else if errorMessage}
-		<p class="text-sm text-red-500">{errorMessage}</p>
-	{/if}
-</form>
+
+		{#if result}
+			<div class="w-full rounded-lg bg-white/10 p-4 text-center">
+				<p class="text-lg font-semibold">{result['Region Espoir']}</p>
+				<p class="text-sm text-gray-500">{result.Ville}</p>
+			</div>
+		{:else if errorMessage}
+			<p class="text-sm text-red-500">{errorMessage}</p>
+		{/if}
+	</form>
+</div>
