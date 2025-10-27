62941641033be1d247c2846fa4b597a375fcad77
Régis Tremblay Lefrançois <regg00@gmail.com>
2025-10-27 16:10:20 -0400

    Updates

---
+++ _app/immutable/chunks/BHfdZbbR.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1rkw721?.base??"/vbq-lookup",a=globalThis.__sveltekit_1rkw721?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/H80HZ4OB.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1vfxx0c?.base??"/vbq-lookup",a=globalThis.__sveltekit_1vfxx0c?.assets??s??"";export{a,s as b};
